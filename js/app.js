7af7c07972af8dd79462594c2f09d16ba3c1f14a
Stephanie Obeytek <sobeytek@gmail.com>
2018-06-13 14:47:43 -0500

    git lab homework

---
+++ js/app.js
@@ -0,0 +1,10 @@
+console.log("JavaScript is running!");
+
+// 1. What command do you use to setup a git repository inside of your folder?
+	// "git clone + repository"
+
+// 2. What command do you use to ask git to start tracking a file?
+	// "Git add"
+
+// 3. What command do you use to ask git to move your file from the staging area to the repository?
+	// "git push origin master"
